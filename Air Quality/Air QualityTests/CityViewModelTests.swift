1ba720b9a3e640eb1389b1e8eae32548511d3a4c
Anthony Hoang <anthony.hoang@slalom.com>
2018-01-25 18:34:02 -0600

    add test for ciity view model

---
+++ Air Quality/Air QualityTests/CityViewModelTests.swift
@@ -0,0 +1,36 @@
+//
+//  CityViewModel.swift
+//  Air QualityTests
+//
+//  Created by Anthony Hoang on 1/25/18.
+//  Copyright © 2018 Anthony Hoang. All rights reserved.
+//
+
+@testable import Air_Quality
+import XCTest
+import OHHTTPStubs
+import SwiftyJSON
+
+class CityViewModelTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+
+    func testNumberFormatting() {
+        let data: [String : Any] = ["city" : "Chicago", "country" : "US", "count" : 678921]
+        let json = JSON(data)
+        if let city = City(json: json) {
+            let viewModel = CityViewModel(city: city)
+            XCTAssertEqual(viewModel.measurements ?? "", "Number of measurements: 678,921")
+        } else {
+            XCTFail()
+        }
+    }
+}
